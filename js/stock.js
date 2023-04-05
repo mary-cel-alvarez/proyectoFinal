d9b47dbd78416b32b5cec34a0afad6c0fca8b669
Maria Alvarez <mary.cel.alvarez92@gmail.com>
2023-04-04 22:55:04 -0300

    Proyecto Final - Primer carga

---
+++ js/stock.js
@@ -0,0 +1,8 @@
+let stockProductos = [
+    {id: 1, nombre:"Alfombra Oval" , cantidad: 1, precio: 1500, img: 'img/alfombras/afombra-oval.jpg'},
+    {id: 2, nombre:"Alfombra Asilash" , cantidad: 1, precio: 1800, img: 'img/alfombras/alfombra-asilash.png'},
+    {id: 3, nombre:"Alfombra Rectangular" , cantidad: 1, precio: 1400, img: 'img/alfombras/alfombra-blancoNegro.jpg'},
+    {id: 4, nombre:"Alfombra Paris" , cantidad: 1, precio: 1300, img: 'img/alfombras/alfombra-paris.png'},
+    {id: 5, nombre:"Alfombra Sumantra" , cantidad: 1, precio: 2000, img: 'img/alfombras/alfombra-sumantra.png'},
+    {id: 6, nombre:"Felpudo Home" , cantidad: 1, precio: 1500, img: 'img/alfombras/felpudo-home.jpg'},
+]
